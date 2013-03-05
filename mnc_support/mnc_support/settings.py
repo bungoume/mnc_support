6f639bce26e251ca49383bf1fd95b85d99d3a901
y.umezaki <bungoume3@yahoo.co.jp>
2013-03-06 01:48:31 +0900

    change settings

--- mnc_support/mnc_support/settings.py
+++ mnc_support/mnc_support/settings.py
@@ -72,7 +72,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    './static/',
+    './static',
 )
 
 # List of finder classes that know how to find static files in
@@ -122,9 +122,10 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'daybook',
 )
 
 # A sample logging configuration. The only tangible logging
