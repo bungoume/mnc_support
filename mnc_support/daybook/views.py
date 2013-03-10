dd60579554ee7c18bf914d3981e9b9d6898c58c9
y.umezaki <bungoume3@yahoo.co.jp>
2013-03-10 18:10:12 +0900

    update crawler

--- mnc_support/daybook/views.py
+++ mnc_support/daybook/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Create your views here.
 
 from django.core import serializers
@@ -46,38 +47,56 @@ def showLesson(request, id):
     return render_to_response('lesson.html', {'lessons': lessons, 'class_info':class_info})
 
 
+#crawler
 def updateClassList(request):
     from pyquery import PyQuery as pq
-    def is_end(html):
+    
+    def getSyllabusData(url):
+        html = fetch(url)
         d = pq(html)
-        return d('.next').length == 0
-    def extract(html):
+        td = d('.ct-sirabasu:first td')
+        classroom = td.eq(8).text()
+
+        obj = {
+            'name': td.eq(2).find('div').text(),
+            'other_name' : '',
+            'class_number' : int(td.eq(11).text()),
+            'teacher' : td.eq(3).text(),
+            #年度 学期
+            'year' : int(td.eq(0).text()[:4]),
+            'term' : 'previous' if td.eq(4).text().find(u'春') != -1 else 'later',
+            #曜日 時限 教室
+            'weekday' : td.eq(4).text(),
+            'hour' : td.eq(4).text(),
+            'classroom' : classroom,
+            'ta_support' : '',
+            'class_key' : int(td.eq(10).text()),
+            'syllabus_url' : url,
+            'keyword' : ''
+        }
+        print obj
+        return obj
+
+    def getSyllabusUrlList():
+        url = "http://www.waseda.jp/mnc/SYLLABUS/2012/kamoku_list.html"
+        html = fetch(url)
         d = pq(html)
-        list = []
-        for tr in d(''):
-            ar = map((lambda input:(input.name[:-2], input.value)),pq(tr).nextAll()[0:8])
-            if len(ar) == 8:
-                list.append(dict(ar))
-        return list
-    def fetch(page):
-        url = "http://1"
-        if(page):
-            url += ""
+        return map(lambda x: pq(x).attr("href"), d('a.external'))
+
+
+    def fetch(url):
+        import urllib2, time
         opener = urllib2.build_opener()
         html = opener.open(url).read()
+        time.sleep(1)
         return html
 
-    classList = []
-    p = 1
-    while 1:
-        print p
-        html = fetch(p)
-        classList.extend(extract(html))
-        if is_end(html):
-            break
-        p += 1
-        time.sleep(1)
-    setClassList(classList)
+    urlList = getSyllabusUrlList()
+    syllabusList = map(getSyllabusData, urlList[:10])
+    import json
+    f = open('text.txt', 'w') # 書き込みモードで開く
+    f.write(json.dumps(syllabusList)) # 引数の文字列をファイルに書き込む
+    f.close()
     return 'ok'
 
 
